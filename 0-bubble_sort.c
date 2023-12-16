bc6e17e15d7af2d13ca04fb39194569f4dd56a06
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-12-16 15:39:46 -0600

    Betty Corrections

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,35 +1,41 @@
+#include <stdio.h>
 #include "sort.h"
+
 /**
- * bubble_sort - sorts an array of integers in ascending order using the Bubble sort algorithm
+ * bubble_sort - sorts an array of integers in ascending order using
+ * the Bubble sort algorithm
  * @array: the array to be sorted
  * @size: the size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-	for (size_t i = 0; i < size - 1; i++)
-{
-	for (size_t j = 0; j < size - i - 1; j++)
+	size_t i, j, k;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
 	{
-	if (array[j] > array[j + 1])
+		for (j = 0; j < size - i - 1; j++)
 		{
-		int temp = array[j];
-		array[j] = array[j + 1];
-		array[j + 1] = temp;
-	  }
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+
+		for (k = 0; k < size; k++)
+		{
+			printf("%d ", array[k]);
+		}
+		printf("\n");
 	}
-	
-	for (size_t k = 0; k < size; k++)
-	{
-	  printf("%d ", array[k]);
-	}
-	printf("\n");
-  }
 }
 
 int my_main(void)
 {
-  int array[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-  size_t size = sizeof(array) / sizeof(array[0]);
-  bubble_sort(array, size);
-  return 0;
+	int array[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+	size_t size = sizeof(array) / sizeof(array[0]);
+	bubble_sort(array, size);
+	return (0);
 }
